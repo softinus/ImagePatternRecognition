e758f2c16ddc2a5895d6f0c02ce0d68169aa14b1
Mark Choi <admin@raimsoft.com>
2018-10-14 11:23:43 +0900

    A

---
+++ ImagePattern/MainForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ImagePattern
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSample = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.pnViewer = new System.Windows.Forms.Panel();
+            this.pictureBoxIpl = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.panel1.SuspendLayout();
+            this.pnViewer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSample);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.btnLoad);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(170, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(91, 282);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnSample
+            // 
+            this.btnSample.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSample.Location = new System.Drawing.Point(0, 199);
+            this.btnSample.Name = "btnSample";
+            this.btnSample.Size = new System.Drawing.Size(91, 83);
+            this.btnSample.TabIndex = 6;
+            this.btnSample.Text = "Sample";
+            this.btnSample.UseVisualStyleBackColor = true;
+            this.btnSample.Click += new System.EventHandler(this.btnSample_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStart.Location = new System.Drawing.Point(0, 92);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(91, 107);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLoad.Location = new System.Drawing.Point(0, 0);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(91, 92);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // pnViewer
+            // 
+            this.pnViewer.Controls.Add(this.pictureBoxIpl);
+            this.pnViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnViewer.Location = new System.Drawing.Point(0, 0);
+            this.pnViewer.Name = "pnViewer";
+            this.pnViewer.Size = new System.Drawing.Size(170, 282);
+            this.pnViewer.TabIndex = 3;
+            // 
+            // pictureBoxIpl
+            // 
+            this.pictureBoxIpl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxIpl.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxIpl.Name = "pictureBoxIpl";
+            this.pictureBoxIpl.Size = new System.Drawing.Size(170, 282);
+            this.pictureBoxIpl.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxIpl.TabIndex = 0;
+            this.pictureBoxIpl.TabStop = false;
+            this.pictureBoxIpl.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxIpl_Paint);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 282);
+            this.Controls.Add(this.pnViewer);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.pnViewer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSample;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Panel pnViewer;
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl;
+
+    }
+}
+
---
+++ ImagePattern/MainForm.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using OpenCvSharp;
+
+namespace ImagePattern
+{
+    public partial class MainForm : Form
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr hWnd01, int hWnd02, string lpsz01, string lpsz02);
+        [DllImport("user32.dll")]
+        public static extern bool GetClientRect(IntPtr hWnd, ref RECT lpRect);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDC(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, ref RECT lpRect);
+
+        IntPtr m_hWndMain = new IntPtr();
+        IntPtr m_hWndCanvas = new IntPtr();
+        RECT m_rcLocation = new RECT();
+        RECT m_rcSize = new RECT();
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        [DllImport("user32.dll")]
+        public static extern int SetCursorPos(int x, int y);
+
+        private const int WM_LBUTTONDOWN = 0x0002;
+        private const int WM_LBUTTONUP = 0x0004;
+        private const int WM_LBUTTONDBLCLK = 0x203;
+
+        Point m_ptStartDot = new Point(0, 0);
+        Bitmap bmpScreen;
+
+        List<IplImage> m_ltPatterns = new List<IplImage>();
+        List<Point> m_ltPoints = new List<Point>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void Load()
+        {
+            m_hWndMain = FindWindow(null, "Mobizen");
+
+            GetWindowRect(m_hWndMain, ref m_rcLocation); // Top, Left
+            GetClientRect(m_hWndMain, ref m_rcSize); // Bottom, Right
+
+            if (m_hWndMain == IntPtr.Zero)
+                return;
+
+            m_ptStartDot = new Point((m_rcLocation.left), (m_rcLocation.top));
+
+            m_ltPatterns.Add(new IplImage("Pattern01.bmp", LoadMode.GrayScale));
+            m_ltPatterns.Add(new IplImage("Pattern02.bmp", LoadMode.GrayScale));
+            m_ltPatterns.Add(new IplImage("Pattern03.bmp", LoadMode.GrayScale));
+            m_ltPatterns.Add(new IplImage("Pattern04.bmp", LoadMode.GrayScale));
+            m_ltPatterns.Add(new IplImage("Pattern05.bmp", LoadMode.GrayScale));
+            m_ltPatterns.Add(new IplImage("Pattern06.bmp", LoadMode.GrayScale));
+            m_ltPatterns.Add(new IplImage("Pattern07.bmp", LoadMode.GrayScale));
+
+            foreach (IplImage iplTemp in m_ltPatterns)
+            {
+                Cv.Threshold(iplTemp, iplTemp, 200, 255, ThresholdType.Binary);
+            }
+
+            
+        }
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            Load();
+        }
+
+        private void Start()
+        {
+            if (m_hWndMain == IntPtr.Zero)
+                return;
+
+            Size szScreen = new Size(m_rcSize.right, m_rcSize.bottom);
+            Bitmap bmpScreen = new Bitmap(szScreen.Width, szScreen.Height, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bmpScreen);
+            g.CopyFromScreen(m_rcLocation.left, m_rcLocation.top, 0, 0, szScreen);
+
+            string text = DateTime.Now.Millisecond.ToString() + "\r\n";
+            bmpScreen.Save(DateTime.Now.Millisecond.ToString()+".bmp");
+
+            IplImage imgConvertBitmap = IplImage.FromBitmap(bmpScreen);
+            IplImage imgMain = new IplImage(new CvSize(imgConvertBitmap.Width, imgConvertBitmap.Height), BitDepth.U8, 1);
+
+            Cv.CvtColor(imgConvertBitmap, imgMain, ColorConversion.RgbToGray);
+            Cv.Threshold(imgMain, imgMain, 200, 255, ThresholdType.Binary);
+
+            foreach (IplImage iplTemp in m_ltPatterns)
+            {
+                int iWidth = imgMain.Width - iplTemp.Width + 1;
+                int iHeight = imgMain.Height - iplTemp.Height + 1;
+
+                IplImage imgResult = new IplImage(Cv.Size(iWidth, iHeight), BitDepth.F32, 1);
+                Cv.MatchTemplate(imgMain, iplTemp, imgResult, MatchTemplateMethod.CCoeffNormed);
+
+                double dMin, dMax;
+                CvPoint ptLeftTop;
+                CvPoint ptRightBottom;
+                Cv.MinMaxLoc(imgResult, out dMin, out dMax, out ptRightBottom, out ptLeftTop);
+
+                m_ltPoints.Add(new Point(m_ptStartDot.X + ptLeftTop.X + 15, m_ptStartDot.Y + ptLeftTop.Y + 15));
+                
+                
+            }
+
+            //
+            //102 369
+
+            SetCursorPos(m_ptStartDot.X, m_ptStartDot.Y);
+
+            
+            foreach (Point ptLocation in m_ltPoints)
+            {
+                text+= "X : " + ptLocation.X + ", " + ptLocation.Y + "\r\n";
+
+                SetCursorPos(ptLocation.X, ptLocation.Y);
+
+                Thread.Sleep(72);
+
+                mouse_event(WM_LBUTTONDOWN, 0, 0, 0, 0);
+
+                Thread.Sleep(65);
+
+                mouse_event(WM_LBUTTONUP, 0, 0, 0, 0);
+
+                Thread.Sleep(65);
+            }
+
+            pictureBoxIpl.ImageIpl = imgMain;
+
+            pictureBoxIpl.Invalidate();
+
+            System.IO.File.WriteAllText("Recent_crood.txt", text);  // 로그 쓴다
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void btnSample_Click(object sender, EventArgs e)
+        {
+            m_ltPoints.Clear();
+            m_ltPatterns.Clear();
+
+            Bitmap bmpScreen = new Bitmap("575.bmp");
+            IplImage imgConvertBitmap = IplImage.FromBitmap(bmpScreen);
+            IplImage imgMain = new IplImage(new CvSize(imgConvertBitmap.Width, imgConvertBitmap.Height), BitDepth.U8, 1);
+            Cv.CvtColor(imgConvertBitmap, imgMain, ColorConversion.RgbToGray);
+            Cv.Threshold(imgMain, imgMain, 210, 255, ThresholdType.Binary);
+
+            m_ltPatterns.Add(new IplImage("Pattern01.bmp", LoadMode.GrayScale));
+            m_ltPatterns.Add(new IplImage("Pattern02.bmp", LoadMode.GrayScale));
+            m_ltPatterns.Add(new IplImage("Pattern03.bmp", LoadMode.GrayScale));
+            m_ltPatterns.Add(new IplImage("Pattern04.bmp", LoadMode.GrayScale));
+            m_ltPatterns.Add(new IplImage("Pattern05.bmp", LoadMode.GrayScale));
+            m_ltPatterns.Add(new IplImage("Pattern06.bmp", LoadMode.GrayScale));
+            m_ltPatterns.Add(new IplImage("Pattern07.bmp", LoadMode.GrayScale));
+
+            foreach (IplImage iplTemp in m_ltPatterns)
+            {
+                Cv.Threshold(iplTemp, iplTemp, 210, 255, ThresholdType.Binary);
+            }
+
+            foreach (IplImage iplTemp in m_ltPatterns)
+            {
+                int iWidth = imgMain.Width - iplTemp.Width + 1;
+                int iHeight = imgMain.Height - iplTemp.Height + 1;
+
+                IplImage imgResult = new IplImage(Cv.Size(iWidth, iHeight), BitDepth.F32, 1);
+                Cv.MatchTemplate(imgMain, iplTemp, imgResult, MatchTemplateMethod.CCorrNormed);
+
+                double dMin, dMax;
+                CvPoint ptLeftTop;
+                CvPoint ptRightBottom;
+                Cv.MinMaxLoc(imgResult, out dMin, out dMax, out ptRightBottom, out ptLeftTop);
+                //cvRectangle(A, left_top, cvPoint(left_top.x + B->width, left_top.y + B->height), CV_RGB(255, 0, 0)); 
+                Cv.Rectangle(imgMain, ptLeftTop, new CvPoint(ptLeftTop.X + iplTemp.Width, ptLeftTop.Y + iplTemp.Height), Cv.RGB(255, 255, 255));
+
+                m_ltPoints.Add(new Point(ptLeftTop.X, ptLeftTop.Y));
+            }
+
+            pictureBoxIpl.ImageIpl = imgMain;
+
+            pictureBoxIpl.Invalidate();
+        }
+
+        private void pictureBoxIpl_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            Font drawFont = new Font("Arial", 14, System.Drawing.FontStyle.Bold);
+            SolidBrush drawBrush = new SolidBrush(Color.Red);
+
+            if (pictureBoxIpl.Image == null)
+                return;
+
+            float fWidthRate = (float)pictureBoxIpl.Size.Width / (float)pictureBoxIpl.Image.Size.Width;
+            float fHeightRate = (float)pictureBoxIpl.Size.Height / (float)pictureBoxIpl.Image.Size.Height;
+
+            int iIndex = 0;
+            foreach (PointF ptLocation in m_ltPoints)
+            {
+                iIndex++;
+
+                g.DrawString(iIndex.ToString(), drawFont, drawBrush, new PointF(ptLocation.X * fWidthRate, ptLocation.Y * fHeightRate));
+            }
+        }
+    }
+}
